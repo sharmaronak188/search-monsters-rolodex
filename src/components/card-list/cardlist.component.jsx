23e77669de34d4627c7c83b9fbfe6c72789b6e34
sharmaronak188 <sharmaronak188@gmail.com>
2022-01-23 15:46:35 +0530

    Updated card-list and card component

--- src/components/card-list/cardlist.component.jsx
+++ src/components/card-list/cardlist.component.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Card } from '../card/card.component';
 import './cardlist.styles.css'
 
 export const CardList = (props) => {
-    return <div className="card-list">{props.children}</div>;
+    return <div className="card-list">
+        {props.monsters.map((monster) => (
+            <Card key={monster.id} monster={monster} />
+        ))}
+    </div>;
 }
